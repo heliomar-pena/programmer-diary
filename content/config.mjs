070ea1a3a0f4edeb6780519eaea01b6caa9dd76f
heliomar-pena <penaheliomar@gmail.com>
2024-07-02 18:54:03 -0400

    added config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,5 +1,13 @@
-
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+const config = {
+  title: "Programmer's Diary",
+  description: "A blog about programming and web development from the perspective of a self-taught developer.",
+  author: "Heliomar Peña",
+  domain: "https://programmers-diary.vercel.app/",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/blog", name: "Blog" }],
+};
+
+export default config;
